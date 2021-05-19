995103fbffb102d83a0991b508dcc0af3ea9dfb1
Giorgio <giorgio.pignattari@gmail.it>
2021-05-19 17:58:43 +0200

    tabella popolata con faker, esercizio terminato

--- laravel-migration-seeder/database/seeds/LaravelMigrationSeederTableSeeder.php
+++ laravel-migration-seeder/database/seeds/LaravelMigrationSeederTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\LaravelMigrationSeeder;
 
 class LaravelMigrationSeederTableSeeder extends Seeder
 {
@@ -9,8 +11,17 @@ class LaravelMigrationSeederTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+      for ($i=0; $i < 10; $i++) {
+        $LaravelMigrationSeeder_object = new LaravelMigrationSeeder();
+        $LaravelMigrationSeeder_object->date = $faker->date();
+        $LaravelMigrationSeeder_object->destination = $faker->city();
+        $LaravelMigrationSeeder_object->country = $faker->state();
+        $LaravelMigrationSeeder_object->people = $faker->numberBetween(0, 100);
+        $LaravelMigrationSeeder_object->category = $faker->randomElement(['Beach', 'Mountain', 'Hill', 'Forest', 'Desert', 'Canyon']);
+        $LaravelMigrationSeeder_object->price = $faker->numberBetween(300, 5000);
+        $LaravelMigrationSeeder_object->save();
+      }
     }
 }
